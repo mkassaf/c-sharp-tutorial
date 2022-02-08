235e0e4896078f19e17101835ced89e4d395cd57
Mustafa Assaf <massaf@cisco.com>
2022-02-08 23:32:32 +0200

    * FunctionInCsharp.sln: * FunctionInCsharp.csproj:
    * Program.cs: Function in C sharp

---
+++ FunctionInCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunctionInCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
