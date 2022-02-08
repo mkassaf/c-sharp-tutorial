45712fb0a1b3d24fe68b6dc9657ba74c64f50c0f
Mustafa Assaf <massaf@cisco.com>
2022-02-08 23:51:50 +0200

    * Functions.cs:
    * Program.cs: add examples

--- FunctionInCsharp/Program.cs
+++ FunctionInCsharp/Program.cs
@@ -6,7 +6,21 @@ namespace FunctionInCsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Functions functions = new Functions();
+            functions.Print("Mustafa");
+            int value = 5;
+            Console.WriteLine("value before CallByValue is " + value);
+            functions.CallByValue(value);
+            Console.WriteLine("value after CallByValue is " + value);
+
+            Console.WriteLine("value before CallByRefrence is " + value);
+            functions.CallByRefrence(ref value);
+            Console.WriteLine("value after CallByRefrence is " + value);
+
+            int unassignedValue;
+            //Console.WriteLine("unassignedValue before CallFunUsingOutParam is " + unassignedValue);// Error: CS0165: Use of unassigned local variable 'unassignedValue'
+            functions.CallFunUsingOutParam(out unassignedValue);
+            Console.WriteLine("unassignedValue after CallFunUsingOutParam is " + unassignedValue);
         }
     }
 }
