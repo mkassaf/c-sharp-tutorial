a7e8b8206d06f60120e4483cd58b81c320b979e6
Mustafa Assaf <massaf@cisco.com>
2022-02-23 19:04:22 +0200

    Delegates

---
+++ Delegates/DelegatesExample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Delegates
+{
+    public class DelegatesExample
+    {
+        public delegate void CleanDelegate();
+
+        public CleanDelegate CleanDelegateFun;
+        public DelegatesExample()
+        {
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Start executing");
+            Thread.Sleep(1000);
+            Console.WriteLine("Finish executing");
+            CleanUp();
+        }
+
+        private void CleanUp()
+        {
+            if(CleanDelegateFun != null)
+            {
+                CleanDelegateFun();
+            }
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegatesExample delegatesExample = new DelegatesExample();
+            delegatesExample.CleanDelegateFun += Clean;
+            delegatesExample.CleanDelegateFun += End;
+            delegatesExample.Execute();
+        }
+
+        public static void End()
+        {
+            Console.WriteLine("End ... ");
+        }
+        public static void Clean()
+        {
+            Console.WriteLine("Clean ... ");
+        }
+    }
+}
