80a038db41e1d87b291f3ce43f111a926a17e830
Mustafa Assaf <massaf@cisco.com>
2022-03-02 18:55:35 +0200

    Exception exmaple

---
+++ ExceptionExmaple/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExceptionExmaple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
